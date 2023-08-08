6a3d6e3114545db1263af7c6ee408fd8dedfc213
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2023-08-08 22:35:54 +0200

    link config>settings to checkparams.js

--- js/checkparams.js
+++ js/checkparams.js
@@ -4,19 +4,24 @@ const portraitwarning = require("./portraitwarning.js");
 const flickerloop = require("./flickerloop.js");
 const favicon = require("./favicon.js");
 const glitch = require("./glitch-effekt.js");
+const { settings } = require("../config.json");
 
 function checkparams(params, document, window) {
-    if (params.noSubheading) {
+    if (params.noSubheading || !settings.subheading) {
         document.getElementById("subheading").style.visibility = "hidden";
         document.getElementById("underscore").style.visibility = "hidden";
     }
-    if (!params.noUnderscore && !params.noSubheading) underscoreLoop();
-    if (!params.noFlicker) flickerloop(document);
-    if (params.noTyping) runTypewriter(document, false);
+    if (!params.noUnderscore && !params.noSubheading && settings.subheading && settings.underscore) underscoreLoop();
+    if (params.noFlicker || !settings.flicker) {
+    } else flickerloop(document);
+    if (params.noTyping || params.noSubheading || !settings.typing || !settings.subheading) runTypewriter(document, false);
     else runTypewriter(document, true);
-    if (!params.noWarning) portraitwarning(document, window);
-    if (!params.noFavicon) favicon(document);
-    if (!params.noGlitch) glitch(document);
+    if (params.noWarning || !settings.warning) {
+    } else portraitwarning(document, window);
+    if (params.noFavicon || !settings.favicon) {
+    } else favicon(document);
+    if (params.noGlitch || !settings.glitch) {
+    } else glitch(document);
 }
 
 module.exports = checkparams;
